7c11bd390dac634f14a21f93d731dba9cde108fc
lactua <minedrayxio@gmail.com>
2024-01-29 22:32:28 +0100

    Update calcpi.py

--- calcpi.py
+++ calcpi.py
@@ -1,8 +1,31 @@
 from decimal import Decimal
+from time import time
 
 limit = eval(input('Limit > '))
+
+def chronometer(func):
+    def wrapper(*args, **kwargs):
+        before = time()
+        _return = func(*args, **kwargs)
+        after = time()
+        
+        return (after - before, _return)
+    return wrapper
 
 def sqrt(n):
     return n**Decimal(1/2)
 
-print(2 * sum(sqrt((sqrt(limit**2 - (x + 1)**2) - sqrt(limit**2 - x**2))**2 + 1) for x in range(limit)) / limit)
+@chronometer
+def calc():
+    f = lambda x: sqrt((sqrt(limit**2 - (x + 1)**2) - sqrt(limit**2 - x**2))**2 + 1)
+
+    _sum = 0
+    for x in range(limit):
+        _sum += f(x)
+        print("{}%".format(round((x+1)/limit*100)), end="\r")
+
+    return 2 * _sum / limit
+
+duration, result = calc()
+print(f"approximation : {result}")
+print(f"took {round(duration, 2)} seconds")
